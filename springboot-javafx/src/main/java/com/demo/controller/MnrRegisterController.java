49b20596cdcb1cce146c5f8bdeb37934e2e2bd61
A04M2320118\sherrywu <hsuan.wu9211@gmail.com>
2024-12-10 13:53:54 +0800

    增加第一支api 的controller & model程式碼

--- springboot-javafx/src/main/java/com/demo/controller/MnrRegisterController.java
+++ springboot-javafx/src/main/java/com/demo/controller/MnrRegisterController.java
@@ -1,18 +1,49 @@
 package com.demo.controller;
 
+import com.demo.models.Resident;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MnrRegisterController {
 
     @FXML
+    private TextField passportNoField;
+    @FXML
+    private TextField permitNoField;
+    @FXML
+    private TextField terminalIdField;
+    @FXML
+    private DatePicker applyDateSField;
+    @FXML
+    private DatePicker applyDateEField;
+    @FXML
+    private TextField travelIdField;
+    @FXML
     private Label mnrResult;
+
 
     @FXML
     protected void onSearchButtonClick() {
-//        welcomeText.setText("[controllerText] Welcome to JavaFX Application!");
+        // 把key in的資料 傳送參數到第一支api
+        // 接收api回應參數 並顯示結果
+        Resident resident = new Resident();
+
+        resident.setPassportNo(passportNoField.getText());
+        resident.setPermitNo(permitNoField.getText());
+        String[] terminalIds = terminalIdField.getText().split(",");
+//        for(String id : terminalIds){
+//          resident.setTerminalId(id);
+//        }
+        if (applyDateSField.getValue() != null) {
+          resident.setApplyDateS(applyDateSField.getValue().toString());
+        }
+        if (applyDateEField.getValue() != null) {
+          resident.setApplyDateE(applyDateEField.getValue().toString());
+        }
+        System.out.println(resident.getPassportNo());
         mnrResult.setText("register還沒開發完~");
     }
-
 
 }
--- springboot-javafx/src/main/java/com/demo/models/Resident.java
+++ springboot-javafx/src/main/java/com/demo/models/Resident.java
@@ -1,17 +1,12 @@
 package com.demo.models;
 
-import lombok.*;
+import java.util.Arrays;
 
-@Data
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Resident {
 
     private String passportNo;  // 護照號碼
     private String permitNo;    // 許可證號
-    private String documentType;    // 證件類別 非必填 預設空字串
+    private String documentType;    // 證件類別
     private String[] terminalId; // 閘門編號
     private String applyDateS;  // 註冊時間-起
     private String applyDateE;  // 註冊時間-迄
@@ -20,4 +15,99 @@ public class Resident {
     private int pageSize;       // 每頁大小
     private int pageNumber;     // 頁碼
 
+    public Resident() {
+        this.documentType = ""; // 預設 documentType 為空字串
+    }
+
+    public Resident(String passportNo, String permitNo, String documentType, String[] terminalId,
+                    String applyDateS, String applyDateE, String travelId, int total, int pageSize, int pageNumber) {
+        this.passportNo = passportNo;
+        this.permitNo = permitNo;
+        this.documentType = documentType;
+        this.terminalId = terminalId;
+        this.applyDateS = applyDateS;
+        this.applyDateE = applyDateE;
+        this.travelId = travelId;
+        this.total = total;
+        this.pageSize = pageSize;
+        this.pageNumber = pageNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Resident{" +
+                "passportNo='" + passportNo + '\'' +
+                ", permitNo='" + permitNo + '\'' +
+                ", documentType='" + documentType + '\'' +
+                ", terminalId=" + Arrays.toString(terminalId) +
+                ", applyDateS='" + applyDateS + '\'' +
+                ", applyDateE='" + applyDateE + '\'' +
+                ", travelId='" + travelId + '\'' +
+                ", total=" + total +
+                ", pageSize=" + pageSize +
+                ", pageNumber=" + pageNumber +
+                '}';
+    }
+
+    public String getPassportNo() {
+        return passportNo;
+    }
+    public void setPassportNo(String passportNo) {
+        this.passportNo = passportNo;
+    }
+    public String getPermitNo() {
+        return permitNo;
+    }
+    public void setPermitNo(String permitNo) {
+        this.permitNo = permitNo;
+    }
+    public String getDocumentType() {
+        return documentType;
+    }
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+    public String[] getTerminalId() {
+        return terminalId;
+    }
+    public void setTerminalId(String[] terminalId) {
+        this.terminalId = terminalId;
+    }
+    public String getApplyDateS() {
+        return applyDateS;
+    }
+    public void setApplyDateS(String applyDateS) {
+        this.applyDateS = applyDateS;
+    }
+    public String getApplyDateE() {
+        return applyDateE;
+    }
+    public void setApplyDateE(String applyDateE) {
+        this.applyDateE = applyDateE;
+    }
+    public String getTravelId() {
+        return travelId;
+    }
+    public void setTravelId(String travelId) {
+        this.travelId = travelId;
+    }
+    public int getTotal() {
+        return total;
+    }
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+    public int getPageNumber() {
+        return pageNumber;
+    }
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
 }
