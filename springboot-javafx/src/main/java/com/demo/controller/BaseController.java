e721fc95ebc2632876a3ffbacf30f754f03f883f
sherrywu <hsuan.wu9211@gmail.com>
2024-12-18 16:59:51 +0800

    增加location讀入程式碼

--- springboot-javafx/src/main/java/com/demo/controller/BaseController.java
+++ springboot-javafx/src/main/java/com/demo/controller/BaseController.java
@@ -1,11 +1,20 @@
 package com.demo.controller;
 
+import com.demo.model.EmergencyResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class BaseController {
@@ -16,6 +25,7 @@ public class BaseController {
     public void setContentPane(StackPane contentPane) {
         this.contentArea = contentPane;
     }
+    private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
 
     // 分頁加載方法
     public void loadView(String fxmlFile) {
@@ -29,8 +39,24 @@ public class BaseController {
             }
         } catch (IOException e) {
             System.err.println("-----Error loading FXML:----- " + fxmlFile);
-            e.printStackTrace();
+            logger.error(e.getMessage(), e);
         }
+    }
+
+    private static final String LOCATION_PATH = "src/config/eGateLocation.json";
+    public static String getLocation(){
+        String location = "";
+        try {
+            FileReader fileReader = new FileReader(LOCATION_PATH);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            Gson gson = new Gson();
+            JsonObject jsonObject = gson.fromJson(bufferedReader, JsonObject.class);
+            location = jsonObject.get("eGateLocation").getAsString();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return location;
     }
 
     // 按鈕切換
--- springboot-javafx/src/main/java/com/demo/service/EmergencyRespServiceImpl.java
+++ springboot-javafx/src/main/java/com/demo/service/EmergencyRespServiceImpl.java
@@ -1,17 +1,27 @@
 package com.demo.service;
 
+import com.demo.controller.BaseController;
 import com.demo.model.EmergencyResponse;
 import com.google.gson.Gson;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
 
 public class EmergencyRespServiceImpl implements EmergencyRespService {
 
+    private static final List<String> VALID_LOCATIONS = Arrays.asList("00", "01", "02", "11", "12", "13");
     Gson gson = new Gson();
-    EmergencyResponse emergencyResp = new EmergencyResponse();
 
     @Override
     public String getRequest(String switchSystem, String switchLocation) {
+
+        EmergencyResponse emergencyReq = new EmergencyResponse();
+        emergencyReq.seteGateLocation(BaseController.getLocation());
+        System.out.println( "-------1----emergencyReq--location:"+ emergencyReq.geteGateLocation());
+
+        EmergencyResponse emergencyResp = new EmergencyResponse();
+
         // 固定回傳的內容
         String ip = "";
         try {
@@ -28,19 +38,24 @@ public class EmergencyRespServiceImpl implements EmergencyRespService {
         emergencyResp.setTerminalTime(timestamp);
 
         // todo  取得location
-        if(switchSystem.isEmpty() || switchLocation.isEmpty()) {
+        if(!switchSystem.equals("CHECK") && !switchSystem.equals("MNR")) {
             emergencyResp.setStatus("false");
             // todo  set msg
-            emergencyResp.setMessage("請求參數不可空值");
-            emergencyResp.seteGateLocation("錯誤");
-        } else if(switchSystem.equals("MNR") && !(switchLocation.equals(emergencyResp.geteGateLocation()))) {
-            // MNR & 切換為不同的location
-            emergencyResp.setStatus("true");
-            emergencyResp.seteGateLocation(switchLocation);
+            emergencyResp.setMessage("請求參數錯誤");
+        } else if(switchSystem.equals("MNR")) {
+            if( !VALID_LOCATIONS.contains(switchLocation) && !switchLocation.equals(emergencyReq.geteGateLocation()) ) {
+                // MNR & 切換為不同的location
+                emergencyResp.setStatus("true");
+                emergencyResp.seteGateLocation(switchLocation);
+            }else {
+                emergencyResp.setStatus("false");
+                emergencyResp.setMessage("切換機場代號錯誤");
+            }
+            // todo
         }else if (switchSystem.equals("CHECK")){
             // 回覆現在連線位置，switchLocation 不更改
             emergencyResp.setStatus("true");
-            emergencyResp.seteGateLocation(emergencyResp.geteGateLocation());
+            emergencyResp.seteGateLocation(emergencyReq.geteGateLocation());
         }
 
         // 轉為json 回傳
