0471464df5bb3de050b553b4fb20c2cd7cf5d597
sherrywu <hsuan.wu9211@gmail.com>
2024-12-30 15:16:37 +0800

    定義檢查B專案程式是否啟用

--- springboot-javafx/src/main/java/com/demo/MainApp.java
+++ springboot-javafx/src/main/java/com/demo/MainApp.java
@@ -1,5 +1,8 @@
 package com.demo;
 
+import com.demo.controller.WebSocketClientController;
+import jakarta.websocket.ContainerProvider;
+import jakarta.websocket.WebSocketContainer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,26 +10,51 @@ import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.net.URI;
+
+import static com.demo.util.ProgramCheckerUtil.isProgramRunning;
+
 public class MainApp extends Application {
 
 	private ConfigurableApplicationContext springContext;
 
 	@Override
 	public void init() throws Exception {
-//		// 啟動 Spring 應用程序上下文
 		springContext = new SpringApplicationBuilder(Main.class).run();
-		// todo修改main
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// 使用 Spring 管理的 FXMLLoader
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/static/views/main.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-		stage.setTitle("MNR查詢系統");
-		stage.setScene(scene);
-		stage.show();
+		// TODO
+		// 1. 檢查 B專案的程式是否啟動
+//		if(isProgramRunning("IntelliJ IDEA")) {
+			// 1.2 若啟動 再檢查WebSocket 是否連線成功
+			try {
+				WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+				container.connectToServer(WebSocketClientController.class, URI.create("ws://localhost:8081/ws"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			// 2.1 若連線失敗 重複五次 若失敗 再顯示錯誤畫面
+
+			// 2.2 若連線成功 執行FXML
+			// 使用 Spring 管理的 FXMLLoader
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/static/views/main.fxml"));
+			Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+			stage.setTitle("MNR查詢系統");
+			stage.setScene(scene);
+			stage.show();
+
+//		}else {
+//			// 1.1 若未啟動 顯示錯誤網頁
+//			System.out.println("B專案未啟動");
+//		}
 	}
+
+
+	// 檢查WebSocket
+
+
 
 	@Override
 	public void stop() throws Exception {
