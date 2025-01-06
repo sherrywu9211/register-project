207baa2f2569966b9946ff5b729f580aa48263e2
sherrywu <hsuan.wu9211@gmail.com>
2025-01-06 10:33:15 +0800

    判斷server端是否啟動 ，已啟動與未啟動的相關邏輯 & 顯示錯誤提醒

--- springboot-javafx/src/main/java/com/demo/MainApp.java
+++ springboot-javafx/src/main/java/com/demo/MainApp.java
@@ -1,20 +1,17 @@
 package com.demo;
 
-import com.demo.controller.WebSocketClientViewController;
-import jakarta.websocket.ContainerProvider;
-import jakarta.websocket.WebSocketContainer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.net.URI;
-
 import static com.demo.util.ProgramCheckerUtil.isProgramRunning;
+import static javax.management.remote.JMXConnectorFactory.connect;
 
 public class MainApp extends Application {
 
@@ -28,32 +25,63 @@ public class MainApp extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// TODO
-		// 1. 檢查 B專案的程式是否啟動
-		if(isProgramRunning("LINE")) {
+		// 1. 檢查 server專案的程式是否啟動
+		if (!isProgramRunning("WebsocketApp")) {
+			// 1.1 如果未啟動 則嘗試啟動server端
+			logger.warn("server端未啟動，client端 嘗試啟動server端");
 			try {
-				WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-				container.connectToServer(WebSocketClientViewController.class, URI.create("ws://localhost:8081/ws"));
-			} catch (Exception e) {
-				logger.error("webSocket connect error" + e.getMessage());
+				Process process = Runtime.getRuntime().exec("C:/workspace/websocket-project/target/websocket-project/websocket-project.exe");
+				process.waitFor();
+			}catch (Exception e) {
+				// 嘗試啟動失敗 顯示錯誤畫面
+				logger.error("server端程式 啟動失敗" + e);
+				showErrorAlert(stage, "server端程式 啟動失敗，請稍後再試");
+				return;
 			}
-			// 2.1 若連線失敗 重複五次 若失敗 再顯示錯誤畫面
+		}
+		// 1.2 server程式已啟動
+		logger.info("server端已啟動");
+		// 2. 檢查websSocket連線
+		checkConnection(stage);
+		// 2.2 若連線成功 執行FXML
+		showMainView(stage);
+	}
 
-			// 2.2 若連線成功 執行FXML
-			// 使用 Spring 管理的 FXMLLoader
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/static/views/mainView.fxml"));
-			Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-			stage.setTitle("MNR查詢系統");
-			stage.setScene(scene);
-			stage.show();
+	private int retryCount = 0;
+	private void checkConnection(Stage stage) {
+		retryCount++;
+		logger.info("WebSocket 連線，第 " + retryCount + " 次嘗試連線");
 
-		}else {
-			// 1.1 若未啟動 顯示錯誤網頁
-			System.out.println("B專案未啟動");
+		if (retryCount >= 5) {
+			// 2.1 若連線失敗 重複五次，若失敗 顯示錯誤畫面
+			Platform.runLater(() -> showErrorAlert(stage, "WebSocket連線失敗，請稍後再試。"));
+			logger.error("WebSocket連線5次失敗");
+		} else {
+			try {
+				Thread.sleep(2000); // 等待 2 秒後重試
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
 		}
 	}
 
 
+	private void showMainView(Stage stage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/static/views/mainView.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+		stage.setTitle("MNR查詢系統");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	private void showErrorAlert(Stage stage, String msg) {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("發生錯誤");
+		alert.setHeaderText(null);
+		alert.setContentText(msg);
+		alert.showAndWait();
+		Platform.exit(); // 結束應用程式
+	}
 
 	@Override
 	public void stop() throws Exception {
